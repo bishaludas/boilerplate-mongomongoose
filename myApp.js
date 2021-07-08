0c2c59547b3298f90ee7b0622d095d6a7f0d403c
bishaludas <supertramp.bishal@gmail.com>
2021-07-08 23:11:25 +0545

    schema fixes

--- myApp.js
+++ myApp.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 /*console.log(process.env['MONGO_URI']);*/
 mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
-const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
-const personSchema= new Schema({
-    name: {type: String, required:true},
-    age: Number,
-    favoriteFoods:[String]
-})
-let Person = mongoose.model('Person', personSchema);
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
