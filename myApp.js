8fcebbafaeed67479a0d0f251f8cc8a3b80cfecb
bishaludas <supertramp.bishal@gmail.com>
2021-11-11 23:18:57 +0545

    update person

--- myApp.js
+++ myApp.js
@@ -62,7 +62,14 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById(personId, function (err, data) {
+    if (err) return handleError(err);
+    console.log(data);
+    data.favoriteFoods.push(foodToAdd);
+    data.save();
+    done(null, data);
+  });
+
 };
 
 const findAndUpdate = (personName, done) => {
