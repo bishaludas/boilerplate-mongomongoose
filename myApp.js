8235f0f13708c8a4dd87f02b9cb56504de9af4d6
bishaludas <supertramp.bishal@gmail.com>
2021-11-11 23:51:49 +0545

    find one and update

--- myApp.js
+++ myApp.js
@@ -75,7 +75,7 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  Person.findOneAndUpdate({ name: personName },{age:ageToSet},{ new: true}, function (err, data) {
+  Person.findOneAndUpdate({ name: personName },{$set:{age:ageToSet}} ,{ new: true}, (err, data)=> {
     if (err) return console.log(err);
     done(null, data);
   });
