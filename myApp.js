92f90f7ff05594fa29fa2c6ffddc71054bce8346
Bishal Udash <bishal.udash@wolfmatrix.com>
2021-11-11 17:39:19 +0545

    find by name

--- myApp.js
+++ myApp.js
@@ -36,7 +36,11 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, function (err, data) {
+    if (err) return handleError(err);
+    console.log(data);
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
