815864f6c2a099a3a2987fc123efdaa23d026092
bishaludas <supertramp.bishal@gmail.com>
2021-11-12 00:12:57 +0545

    remove user with name filter

--- myApp.js
+++ myApp.js
@@ -92,7 +92,7 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  Person.remove({name:{ $in: nameToRemove}},(err, data)=> {
+  Person.remove({name: nameToRemove},(err, data)=> {
     if (err) return console.log(err);
     done(null, data);
   });
